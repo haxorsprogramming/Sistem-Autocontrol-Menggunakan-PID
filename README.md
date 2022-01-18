3b673626178a0b94bfe4a0f9295c3143fc6b078b
Haxorsprogramming <haxors@uinsu.ac.id>
2022-01-18 12:00:06 +0700

    'update'

---
+++ README.md
@@ -0,0 +1,1 @@
+## Sistem autocontrol menggunakan metode PID
--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+from time import sleep
 import numpy as np
+import pandas as pd
+
+data_driving = pd.read_excel("data_driving.xlsx")
+
+def PID_Controller(Kp=1, Ti=1, t_initial=0, u_bar=0, control_type='pid'):
+    print("Inisialisasi ...")
+    sleep(1)
+    print("Mulai jalan ..")
+    arr_data = data_driving.to_numpy()
+    # print(arr_data)
+    for x in arr_data:
+        direction = x[1]
+        flow = 'maju'
+
+        if x[1] < x[2]:
+            direction = x[2]
+            flow = "mundur"
+        elif x[2] < x[3]:
+            direction = x[2]
+            flow = "kiri"
+        elif x[3] < x[4]:
+            direction = x[3]
+            flow = "kanan"
+
+        vTemp = car_dynamics(x[1], x[2], x[3], x[4], x[2])
+        print("Result : "+str(vTemp)+" - Arah : "+flow)
+        sleep(0.2)
+
+def car_dynamics(b, v_in, u, m, dt):
+    temp = (-b*v_in + u)/m
+    v_out = temp*dt + v_in
+    return v_out
+
+PID_Controller()
